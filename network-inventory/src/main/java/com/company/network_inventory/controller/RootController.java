4652e243e3c0d2f61664826f1eb6addf8df94d35
nikhil.ar <nikhil.ar@prodapt.com>
2025-12-30 12:02:41 +0530

    Add root controller for health check

---
+++ network-inventory/src/main/java/com/company/network_inventory/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.company.network_inventory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Network Inventory Backend is running";
+    }
+}
